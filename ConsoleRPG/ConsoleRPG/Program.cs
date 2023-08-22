1e4a8c4e3ff0d7d2f2e461895938baaa048aa87a
tjdgh7419 <70570791+tjdgh7419@users.noreply.github.com>
2023-08-22 11:19:43 +0900

    [UPDATE] 던전 기능 추가
    + 쉬움, 일반, 어려움, 스테이지 클리어, 스테이지 실패 추가

--- ConsoleRPG/ConsoleRPG/Program.cs
+++ ConsoleRPG/ConsoleRPG/Program.cs
@@ -207,7 +207,6 @@ namespace ConsoleRPG
 			Console.WriteLine("2. 인벤토리");
 			Console.ResetColor();
 			Console.WriteLine("3. 상점");
-			Console.WriteLine();
 			Console.WriteLine("4. 던전입장");
 			Console.WriteLine();
 
@@ -221,7 +220,7 @@ namespace ConsoleRPG
 					case "1": StateOn(); break;
 					case "2": InventoryOn(); break;
 					case "3": Store(); break;
-					case "4": ; break;
+					case "4": Dungeon(); break;
 					default: Console.WriteLine("잘못된 입력입니다"); break;
 				}
 			}
@@ -233,9 +232,183 @@ namespace ConsoleRPG
 			Console.WriteLine("던전입장");
 			Console.WriteLine("이곳에서 던전으로 들어가기전 활동을 할 수 있습니다.");
 			Console.WriteLine();
-			Console.WriteLine("1. 쉬운 던전");
-			Console.WriteLine("2. 일반 던전");
-			Console.WriteLine("3. 어려운 던전");
+			Console.WriteLine("1. 쉬운 던전     | 방어력 5 이상 권장");
+			Console.WriteLine("2. 일반 던전     | 방어력 11 이상 권장");
+			Console.WriteLine("3. 어려운 던전    | 방어력 17 이상 권장");
+			Console.WriteLine("0. 나가기");
+			Console.WriteLine();
+			Console.WriteLine("원하시는 행동을 입력해주세요.");
+			Console.Write(">>");
+
+			while (true)
+			{
+				string? input = Console.ReadLine();
+				switch (input)
+				{
+					case "1": EasyStage(); break;
+					case "2": NormalStage(); break;
+					case "3": HardStage(); break;
+					case "0": GameStart(); break;
+					default: Console.WriteLine("잘못된 입력입니다"); break;
+				}
+			}
+		}
+
+		public void EasyStage()
+		{
+			Random ClearRan = new Random();
+			Random DownHpRan = new Random();
+			Random GoldRan = new Random();
+			int GoldAtk = GoldRan.Next(player.AttackPower, (player.AttackPower * 2) + 1);
+			int clearChk = ClearRan.Next(1, 11);
+			int DownHp = DownHpRan.Next(20, 36);
+			int PreviousHP = player.Health;
+			int PreviousGold = player.Gold;
+			
+			if (player.DefensivePower < 5)
+			{
+				if(clearChk <= 4)
+				{
+					player.Health -= PreviousHP / 2;
+					StageFail(PreviousHP, "쉬움");
+				}
+				else
+				{
+					int extraGold = 10 * GoldAtk; // 1000/100 = 10
+					player.Gold += 1000 + extraGold;
+					player.Health -= DownHp + (5 - player.DefensivePower); 
+					StageClear(PreviousHP, PreviousGold, "쉬움");
+				}
+			}
+			else
+			{
+				int extraGold = 10 * GoldAtk;
+				player.Gold += 1000 + extraGold;
+				player.Health -= DownHp - (player.DefensivePower - 5);
+				StageClear(PreviousHP, PreviousGold, "쉬움");
+			}
+		}
+
+		public void NormalStage()
+		{
+			Random ClearRan = new Random();
+			Random DownHpRan = new Random();
+			Random GoldRan = new Random();
+			int GoldAtk = GoldRan.Next(player.AttackPower, (player.AttackPower * 2) + 1);
+			int clearChk = ClearRan.Next(1, 11);
+			int DownHp = DownHpRan.Next(20, 36);
+			int PreviousHP = player.Health;
+			int PreviousGold = player.Gold;
+
+			if (player.DefensivePower < 11)
+			{
+				if (clearChk <= 4)
+				{
+					player.Health -= PreviousHP / 2;
+					StageFail(PreviousHP, "일반");
+				}
+				else
+				{
+					int extraGold = 17 * GoldAtk; // 1700/100 = 10
+					player.Gold += 1700 + extraGold;
+					player.Health -= DownHp + (11 - player.DefensivePower);
+					StageClear(PreviousHP, PreviousGold, "일반");
+				}
+			}
+			else
+			{
+				int extraGold = 17 * GoldAtk;
+				player.Gold += 1700 + extraGold;
+				player.Health -= DownHp - (player.DefensivePower - 11);
+				StageClear(PreviousHP, PreviousGold, "일반");
+			}
+		}
+
+		public void HardStage()
+		{
+			Random ClearRan = new Random();
+			Random DownHpRan = new Random();
+			Random GoldRan = new Random();
+			int GoldAtk = GoldRan.Next(player.AttackPower, (player.AttackPower * 2) + 1);
+			int clearChk = ClearRan.Next(1, 11);
+			int DownHp = DownHpRan.Next(20, 36);
+			int PreviousHP = player.Health;
+			int PreviousGold = player.Gold;
+
+			if (player.DefensivePower < 17)
+			{
+				if (clearChk <= 4)
+				{
+					player.Health -= PreviousHP / 2;
+					StageFail(PreviousHP, "어려움");
+				}
+				else
+				{
+					int extraGold = 25 * GoldAtk; // 2500/100 = 10
+					player.Gold += 2500 + extraGold;
+					player.Health -= DownHp + (17 - player.DefensivePower);
+					StageClear(PreviousHP, PreviousGold, "어려움");
+				}
+			}
+			else
+			{
+				int extraGold = 25 * GoldAtk;
+				player.Gold += 2500 + extraGold;
+				player.Health -= DownHp - (player.DefensivePower - 17);
+				StageClear(PreviousHP, PreviousGold, "어려움");
+			}
+		}
+
+		public void StageClear(int PrevHP, int PrevGold, string StageLevel)
+		{
+			Console.Clear();
+			Console.WriteLine("던전 클리어");
+			Console.WriteLine("축하합니다!!");
+			Console.WriteLine($"{StageLevel} 던전을 클리어 하였습니다.");
+			Console.WriteLine();
+			Console.WriteLine("[탐험 결과]");
+			Console.WriteLine($"체력 {PrevHP} -> {player.Health}");
+			Console.WriteLine($"Gold {PrevGold} G -> {player.Gold} G");
+			Console.WriteLine();
+			Console.WriteLine("0. 나가기");
+			Console.WriteLine();
+			Console.WriteLine("원하시는 행동을 입력해주세요.");
+			Console.Write(">>");
+			while (true)
+			{
+				string? input = Console.ReadLine();
+				switch (input)
+				{				
+					case "0": Dungeon(); break;
+					default: Console.WriteLine("잘못된 입력입니다"); break;
+				}
+			}
+		}
+
+		public void StageFail(int PrevHP, string StageLevel) 
+		{
+			Console.Clear();
+			Console.WriteLine("던전 실패");
+			Console.WriteLine($"{player.Name} 가 쓰러졌습니다..");
+			Console.WriteLine($"{StageLevel} 던전을 실패 하였습니다.");
+			Console.WriteLine();
+			Console.WriteLine("[탐험 결과]");
+			Console.WriteLine($"체력 {PrevHP} -> {player.Health}");
+			Console.WriteLine();
+			Console.WriteLine("0. 나가기");
+			Console.WriteLine();
+			Console.WriteLine("원하시는 행동을 입력해주세요.");
+			Console.Write(">>");
+
+			while (true)
+			{
+				string? input = Console.ReadLine();
+				switch (input)
+				{
+					case "0": Dungeon(); break;
+					default: Console.WriteLine("잘못된 입력입니다"); break;
+				}
+			}
 		}
 
 		public void Store()
