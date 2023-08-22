38a3282703cf0c9900e6a6567d4e40c44b5ae0a0
tjdgh7419 <70570791+tjdgh7419@users.noreply.github.com>
2023-08-22 12:14:24 +0900

    [FIX] 황금검 클래스 정리

---
+++ ConsoleRPG/ConsoleRPG/GoldSword.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRPG
+{
+	public class GoldSword : Item
+	{
+		public string Name { get; set; }
+		public int DefensivePower { get; set; }
+		public string Information { get; set; }
+		public int AttackPower { get; set; }
+		public int Price { get; set; }
+		public bool I_Exist { get; set; }
+		public bool equipped { get; set; }
+		public string Sign { get; set; }
+
+		public GoldSword(string Name, int AttackPower, string Information)
+		{
+			this.Name = Name;
+			this.AttackPower = AttackPower;
+			this.Information = Information;
+			DefensivePower = 0;
+			Price = 1300;
+			I_Exist = false;
+			equipped = false;
+			Sign = "";
+		}
+	}
+}
