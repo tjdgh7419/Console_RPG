81cfb9b61eaf9876545b5c62ea0cb9089682461a
tjdgh7419 <70570791+tjdgh7419@users.noreply.github.com>
2023-08-22 12:13:15 +0900

    [FIX] 검 클래스 정리

--- ConsoleRPG/ConsoleRPG/Program.cs
+++ ConsoleRPG/ConsoleRPG/Program.cs
@@ -6,29 +6,6 @@ namespace ConsoleRPG
 	
 
 	
-	public class Sword : Item
-	{
-		public string Name { get; set; }
-		public int DefensivePower { get; set; }
-		public string Information { get; set; }
-		public int AttackPower { get; set; }
-		public int Price { get; set; }
-		public bool I_Exist { get; set; }
-		public bool equipped { get; set; }
-		public string Sign { get; set; }
-		
-		public Sword(string Name, int AttackPower, string Information)
-		{
-			this.Name = Name;
-			this.AttackPower = AttackPower;
-			this.Information = Information;
-			DefensivePower = 0;
-			Price = 800;
-			I_Exist = false;
-			equipped = false;
-			Sign = "";
-		}
-	}
 
 	public class GoldSword : Item
 	{
