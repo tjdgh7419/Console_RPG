57aec0b46cc07ad06820c918155a9ee296149ede
tjdgh7419 <70570791+tjdgh7419@users.noreply.github.com>
2023-08-22 12:06:31 +0900

    [FIX] 플레이어 파일 정리

--- ConsoleRPG/ConsoleRPG/Player.cs
+++ ConsoleRPG/ConsoleRPG/Player.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace ConsoleRPG
 {
-	public class Player
+	public class Player : Character
 	{
+		public int Level { get; set; }
+		public string Name { get; }
+		public string chad { get; }
+		public int AttackPower { get; set; }
+		public int DefensivePower { get; set; }
+		public int Health { get; set; }
+		public int Gold { get; set; }
+
+		public Player(string Name, string chad)
+		{
+
+			Level = 01;
+			this.Name = Name;
+			this.chad = chad;
+			AttackPower = 10;
+			DefensivePower = 5;
+			Health = 100;
+			Gold = 1500;
+		}
+
+		public void PlayerDead()
+		{
+			Console.Clear();
+			Console.WriteLine($"{Name} 이 죽었습니다..");
+		}
 	}
 }
--- ConsoleRPG/ConsoleRPG/Program.cs
+++ ConsoleRPG/ConsoleRPG/Program.cs
@@ -6,34 +6,7 @@ namespace ConsoleRPG
 	// 혹시 몰라서 interface로 구현, 바로 클래스로 구현 가능
 	
 
-	public class Player : Character
-	{
-		public int Level { get; set; }
-		public string Name { get; }
-		public string chad { get; }
-		public int AttackPower { get; set; }
-		public int DefensivePower { get; set; }
-		public int Health { get; set; }
-		public int Gold { get; set; }
-
-		public Player(string Name, string chad)
-		{
-			
-			Level = 01;
-			this.Name = Name;
-			this.chad = chad;
-			AttackPower = 10;
-			DefensivePower = 5;
-			Health = 100;
-			Gold = 1500;
-		}
-
-		public void PlayerDead()
-		{
-			Console.Clear();
-			Console.WriteLine($"{Name} 이 죽었습니다..");
-		}
-	}
+	
 	public class State : Player
 	{
 		public State(string Name, string chad) : base(Name, chad) { }
