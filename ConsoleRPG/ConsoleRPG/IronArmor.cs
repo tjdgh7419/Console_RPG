9123b17a29d76400f77822d99a0832ea77a18ace
tjdgh7419 <70570791+tjdgh7419@users.noreply.github.com>
2023-08-22 12:11:47 +0900

    [FIX] 철반지 클래스 정리

--- ConsoleRPG/ConsoleRPG/IronArmor.cs
+++ ConsoleRPG/ConsoleRPG/IronArmor.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace ConsoleRPG
 {
-	internal class Class1
+	public class IronArmor : Item
 	{
+		public string Name { get; set; }
+		public int DefensivePower { get; set; }
+		public string Information { get; set; }
+		public int AttackPower { get; set; }
+		public int Price { get; set; }
+		public bool I_Exist { get; set; }
+		public bool equipped { get; set; }
+		public string Sign { get; set; }
+		public IronArmor(string Name, int DefensivePower, string Information)
+		{
+			this.Name = Name;
+			this.DefensivePower = DefensivePower;
+			this.Information = Information;
+			AttackPower = 0;
+			Price = 600;
+			I_Exist = true;
+			equipped = false;
+			Sign = "";
+		}
 	}
 }
--- ConsoleRPG/ConsoleRPG/IronRing.cs
+++ ConsoleRPG/ConsoleRPG/IronRing.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace ConsoleRPG
 {
-	internal class Class2
+	public class IronRing : Item
 	{
+		public string Name { get; set; }
+		public int DefensivePower { get; set; }
+		public string Information { get; set; }
+		public int AttackPower { get; set; }
+		public int Price { get; set; }
+		public bool I_Exist { get; set; }
+		public bool equipped { get; set; }
+		public string Sign { get; set; }
+
+		public IronRing(string Name, int DefensivePower, string Information)
+		{
+			this.Name = Name;
+			this.DefensivePower = DefensivePower;
+			this.Information = Information;
+			AttackPower = 0;
+			Price = 400;
+			I_Exist = false;
+			equipped = false;
+			Sign = "";
+		}
 	}
 }
