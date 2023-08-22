90522f15e31b43aa23d5aa137ba9d8f5adb08225
tjdgh7419 <70570791+tjdgh7419@users.noreply.github.com>
2023-08-22 12:11:09 +0900

    [FIX] 철갑옷 클래스 정리

--- ConsoleRPG/ConsoleRPG/Program.cs
+++ ConsoleRPG/ConsoleRPG/Program.cs
@@ -4,28 +4,7 @@ using System;
 namespace ConsoleRPG
 {
 	
-	public class IronArmor : Item
-	{
-		public string Name { get; set; }
-		public int DefensivePower { get; set; }
-		public string Information { get; set; }
-		public int AttackPower { get; set; }
-		public int Price { get; set; }
-		public bool I_Exist { get; set; }
-		public bool equipped { get; set; }
-		public string Sign { get; set; }
-		public IronArmor(string Name, int DefensivePower, string Information) 
-		{ 
-			this.Name = Name;
-			this.DefensivePower = DefensivePower;
-			this.Information = Information;
-			AttackPower = 0;
-			Price = 600;
-			I_Exist = true;
-			equipped = false;
-			Sign = "";
-		}
-	}
+	
 
 	public class IronRing : Item
 	{
