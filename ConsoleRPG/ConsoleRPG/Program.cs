ac440f1a6146a857032a2ea824e72eb26f970e19
tjdgh7419 <70570791+tjdgh7419@users.noreply.github.com>
2023-08-22 23:36:23 +0900

    [FIX] 스테이지 클래스 정리

--- ConsoleRPG/ConsoleRPG/Program.cs
+++ ConsoleRPG/ConsoleRPG/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 
 namespace ConsoleRPG
 {
-	
+
 	public class Start
 	{
 		public static Start _instance = null;
@@ -20,7 +20,6 @@ namespace ConsoleRPG
 
 			return _instance;
 		}
-		
 		public int AttackUP = 0;
 		public int DefensiveUP = 0;
 		public Player player = new State("Sungho", "전사");
@@ -31,14 +30,13 @@ namespace ConsoleRPG
 			  new Sword("검", 4, "튼튼한 검입니다."),
 			  new IronRing("철반지", 3, "은으로 만들어진 반지입니다.")
 			};
-
 		public List<Item> items = new List<Item> { };
 		public Start()
 		{
 			items.Add(store_Items[1]);
 			items.Add(store_Items[2]);
 		}
-		
+
 		public void GameStart()
 		{
 			Enhanc eh = new Enhanc();
@@ -46,6 +44,7 @@ namespace ConsoleRPG
 			DungeonStage ds = new DungeonStage();
 			StoreStage st = new StoreStage();
 			Inventory it = new Inventory();
+			StateStage ss = new StateStage();
 			Console.Clear();
 			Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
 			Console.WriteLine("이곳에서 던전을 들어가기 전 활동을 할 수 있습니다.");
@@ -74,70 +73,22 @@ namespace ConsoleRPG
 				string? input = Console.ReadLine();
 				switch (input)
 				{
-					case "1": StateOn(); break;
+					case "1": ss.StateOn(); break;
 					case "2": it.InventoryOn(); break;
 					case "3": st.Store(); break;
 					case "4": ds.Dungeon(); break;
 					case "5": rs.RestOn(); break;
-					case "6" : eh.EnhanceOn(); break;
-					default: Console.WriteLine("잘못된 입력입니다"); break;
-				}
-			}
-		}			
-
-
-		public void StateOn()
-		{
-			Console.Clear();
-			Console.ForegroundColor = ConsoleColor.Green;
-			Console.WriteLine("상태 보기");
-			Console.ResetColor();
-			Console.WriteLine("캐릭터의 정보가 표시됩니다");
-			Console.WriteLine();
-			Console.WriteLine();
-			Console.WriteLine($"이름 : {player.Name}");
-			Console.WriteLine($"Lv {player.Level}");
-			Console.WriteLine($"Chad ( {player.chad} )");
-			if (AttackUP > 0)
-			{
-				Console.WriteLine($"공격력 : {player.AttackPower} (+{AttackUP})");
-			}
-			else
-			{
-				Console.WriteLine($"공격력 : {player.AttackPower}");
-			}
-			if (DefensiveUP > 0)
-			{
-				Console.WriteLine($"방어력 : {player.DefensivePower} (+{DefensiveUP})");
-			}
-			else
-			{
-				Console.WriteLine($"방어력 : {player.DefensivePower}");
-			}
-			Console.WriteLine($"체 력 : {player.Health}");
-			Console.WriteLine($"Gold : {player.Gold} G");
-			Console.WriteLine();
-			Console.WriteLine("0. 나가기");
-			Console.WriteLine();
-			Console.WriteLine("원하시는 행동을 입력해주세요");
-			Console.WriteLine(">>");
-
-			while (true)
-			{
-				string? input = Console.ReadLine();
-				switch (input)
-				{
-					case "0": GameStart(); break;
+					case "6": eh.EnhanceOn(); break;
 					default: Console.WriteLine("잘못된 입력입니다"); break;
 				}
 			}
 		}
-	}
-	internal class Program
-	{
-		static void Main(string[] args)
+
+		internal class Program
 		{
-			List<Item> store_Items = new List<Item>
+			static void Main(string[] args)
+			{
+				List<Item> store_Items = new List<Item>
 			{ new GoldSword("황금검", 8, "금으로 만들어진 검입니다."),
 			  new IronArmor("무쇠갑옷", 5, "무쇠로 만들어져 튼튼한 갑옷입니다."),
 			  new OldSword("낡은 검", 2, "쉽게 볼 수 있는 낡은 검입니다."),
@@ -145,16 +96,17 @@ namespace ConsoleRPG
 			  new IronRing("철반지", 3, "은으로 만들어진 반지입니다.")
 			};
 
-			List<Item> items = new List<Item>()
-			{ };
+				List<Item> items = new List<Item>()
+				{ };
 
-			items.Add(store_Items[1]);
-			items.Add(store_Items[2]);
+				items.Add(store_Items[1]);
+				items.Add(store_Items[2]);
 
-			Player player = new State("Sungho", "전사");
+				Player player = new State("Sungho", "전사");
 
-			Start start = new Start();
-			start.GameStart();
+				Start start = new Start();
+				start.GameStart();
+			}
 		}
 	}
 }
---
+++ ConsoleRPG/ConsoleRPG/StateStage.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRPG
+{
+	public class StateStage
+	{
+		Start s6 = Start.Instance();
+		public void StateOn()
+		{
+			Console.Clear();
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("상태 보기");
+			Console.ResetColor();
+			Console.WriteLine("캐릭터의 정보가 표시됩니다");
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine($"이름 : {s6.player.Name}");
+			Console.WriteLine($"Lv {s6.player.Level}");
+			Console.WriteLine($"Chad ( {s6.player.chad} )");
+			if (s6.AttackUP > 0)
+			{
+				Console.WriteLine($"공격력 : {s6.player.AttackPower} (+{s6.AttackUP})");
+			}
+			else
+			{
+				Console.WriteLine($"공격력 : {s6.player.AttackPower}");
+			}
+			if (s6.DefensiveUP > 0)
+			{
+				Console.WriteLine($"방어력 : {s6.player.DefensivePower} (+{s6.DefensiveUP})");
+			}
+			else
+			{
+				Console.WriteLine($"방어력 : {s6.player.DefensivePower}");
+			}
+			Console.WriteLine($"체 력 : {s6.player.Health}");
+			Console.WriteLine($"Gold : {s6.player.Gold} G");
+			Console.WriteLine();
+			Console.WriteLine("0. 나가기");
+			Console.WriteLine();
+			Console.WriteLine("원하시는 행동을 입력해주세요");
+			Console.WriteLine(">>");
+
+			while (true)
+			{
+				string? input = Console.ReadLine();
+				switch (input)
+				{
+					case "0": s6.GameStart(); break;
+					default: Console.WriteLine("잘못된 입력입니다"); break;
+				}
+			}
+		}
+	}
+}
+
