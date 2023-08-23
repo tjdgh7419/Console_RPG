f2e94f9f99b0725f75b0bf3a4c2256cc857fe78a
tjdgh7419 <70570791+tjdgh7419@users.noreply.github.com>
2023-08-23 12:20:01 +0900

    [FIX] 플레이어가 죽었을 때 기능 수정

--- ConsoleRPG/ConsoleRPG/Player.cs
+++ ConsoleRPG/ConsoleRPG/Player.cs
@@ -30,8 +30,36 @@ namespace ConsoleRPG
 
 		public void PlayerDead()
 		{
+			Start pd = Start.Instance();
+
 			Console.Clear();
 			Console.WriteLine($"{Name} 이 죽었습니다..");
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine("1. 다시 시작");
+			Console.WriteLine("2. 게임 종료");
+			Console.WriteLine();
+			Console.WriteLine("원하시는 행동을 입력해주세요");
+			Console.WriteLine(">>");
+
+			while (true)
+			{
+				string? input = Console.ReadLine();
+				switch (input)
+				{
+					case "1":
+						pd.player.Level = 1;
+						pd.player.AttackPower = 10.0f;
+						pd.player.DefensivePower = 5.0f;
+						pd.player.Health = 100.0f;
+						pd.player.Gold = 1500;
+						pd.GameStart();
+						break;
+					case "2": return;
+					default: Console.WriteLine("잘못된 입력입니다"); break;
+				}
+			}
+			
 		}
 	}
 }
